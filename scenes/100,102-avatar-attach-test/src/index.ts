49ddec90d7cb3fbf56ce79ec9aade105dd7999ef
Alejandro Alvarez Melucci <alejandro.alvarez@decentraland.org>
2024-05-17 14:38:31 -0300

    Modified avatar-attach-test scene to test all the new avatar attach points

--- scenes/100,102-avatar-attach-test/src/index.ts
+++ scenes/100,102-avatar-attach-test/src/index.ts
@@ -1,59 +1,48 @@
-import { Color4, Quaternion, Vector3 } from '@dcl/ecs-math'
-import { AvatarAnchorPointType, AvatarAttach, Entity, Material, MeshCollider, MeshRenderer, PlayerIdentityData, Transform, engine } from '@dcl/sdk/ecs'
+// We define the empty imports so the auto-complete feature works as expected.
+import { Color4 } from '@dcl/sdk/math'
+import { AvatarAnchorPointType, AvatarAttach, Material, MeshRenderer, Transform, engine } from '@dcl/sdk/ecs'
 
-function createBlock(position: Vector3): Entity {
-  const block = engine.addEntity()
-  Transform.create(block, { position, scale: Vector3.create(3, 2, 3) })
+function addBox(anchor: AvatarAnchorPointType, color: Color4) {
+  const box = engine.addEntity()
+  Transform.create(box,
+    {
+      position: { x: 0, y: 0, z: 0 },
+      scale: { x: 0.15, y: 0.15, z: 0.15 },
+    })
 
-  MeshCollider.setBox(block)
-  MeshRenderer.setBox(block)
-  Material.setPbrMaterial(block, { albedoColor: Color4.create(1.0, 0.0, 0.0, 0.2) , castShadows: false})
-
-  const face = engine.addEntity()
-  Transform.create(face, { position:Vector3.create(0, 0.501, 0), parent: block, rotation: Quaternion.fromEulerDegrees(90,0,0)})
-  MeshRenderer.setPlane(face)
-  Material.setPbrMaterial(face, { albedoColor: Color4.create(1.0, 1.0, 1.0, 0.8), castShadows: false})
-
-  return block
-}
-
-const WithAttached = engine.defineComponent("WithAttached", {})
-
-function createAttached(avatarId: string, anchorPointId: AvatarAnchorPointType, color: Color4, scale: number = 1.0): Entity {
-  const parent = engine.addEntity()
-  const entity = engine.addEntity()
-  Transform.create(entity, { scale: Vector3.create(scale,scale,scale), rotation: Quaternion.fromEulerDegrees(90,0,0), parent})
-
-  MeshRenderer.setPlane(entity)
-  Material.setPbrMaterial(entity, { albedoColor: color , castShadows: false})
-  
-  AvatarAttach.create(parent, { avatarId, anchorPointId })
-
-  return entity
-}
-
-function avatarAttachTest() {
-  createBlock(Vector3.create(1.5, 0, 1.5))
-  createBlock(Vector3.create(2 + 1.5, 1, 1.5))
-
-  engine.addSystem(function() {
-
-    for (const [entity, player] of engine.getEntitiesWith(PlayerIdentityData)) {
-      if (WithAttached.has(entity)) continue
-      
-      createAttached(player.address,  AvatarAnchorPointType.AAPT_POSITION, Color4.Yellow())
-      createAttached(player.address,  AvatarAnchorPointType.AAPT_NAME_TAG, Color4.Green())
-      WithAttached.create(entity)
-
-      const sphere = engine.addEntity()
-      Transform.create(sphere, { scale: Vector3.create(.2,.2,.2), parent: entity})
-      MeshRenderer.setSphere(sphere)
-      Material.setPbrMaterial(sphere, { albedoColor: Color4.Blue() , castShadows: false})
-    }
+  MeshRenderer.setBox(box)
+  AvatarAttach.create(box, {
+    anchorPointId: anchor
   })
 
+  Material.setBasicMaterial(box, {
+    diffuseColor: color,
+  })
 }
 
-export function main() {
-  avatarAttachTest()
-}
+addBox(AvatarAnchorPointType.AAPT_POSITION, Color4.create(1.0, 0.0, 0.0, 1))
+addBox(AvatarAnchorPointType.AAPT_NAME_TAG, Color4.create(0.0, 1.0, 0.0, 1))
+addBox(AvatarAnchorPointType.AAPT_HEAD, Color4.create(0.0, 0.0, 1.0, 1))
+addBox(AvatarAnchorPointType.AAPT_NECK, Color4.create(1.0, 1.0, 0.0, 1))
+addBox(AvatarAnchorPointType.AAPT_SPINE, Color4.create(1.0, 0.0, 1.0, 1))
+addBox(AvatarAnchorPointType.AAPT_SPINE1, Color4.create(0.0, 1.0, 1.0, 1))
+addBox(AvatarAnchorPointType.AAPT_SPINE2, Color4.create(0.5, 0.0, 0.0, 1))
+addBox(AvatarAnchorPointType.AAPT_HIP, Color4.create(0.0, 0.5, 0.0, 1))
+addBox(AvatarAnchorPointType.AAPT_LEFT_SHOULDER , Color4.create(0.0, 0.0, 0.5, 1))
+addBox(AvatarAnchorPointType.AAPT_LEFT_ARM , Color4.create(0.5, 0.5, 0.0, 1))
+addBox(AvatarAnchorPointType.AAPT_LEFT_FOREARM , Color4.create(0.5, 0.0, 0.5, 1))
+addBox(AvatarAnchorPointType.AAPT_LEFT_HAND, Color4.create(0.0, 0.5, 0.5, 1))
+addBox(AvatarAnchorPointType.AAPT_LEFT_HAND_INDEX , Color4.create(0.75, 0.25, 0.5, 1))
+addBox(AvatarAnchorPointType.AAPT_RIGHT_SHOULDER , Color4.create(0.25, 0.75, 0.5, 1))
+addBox(AvatarAnchorPointType.AAPT_RIGHT_ARM , Color4.create(0.5, 0.25, 0.75, 1))C
+addBox(AvatarAnchorPointType.AAPT_RIGHT_FOREARM , Color4.create(0.25, 0.5, 0.75, 1))
+addBox(AvatarAnchorPointType.AAPT_RIGHT_HAND, Color4.create(0.75, 0.5, 0.25, 1))
+addBox(AvatarAnchorPointType.AAPT_RIGHT_HAND_INDEX , Color4.create(0.5, 0.75, 0.25, 1))
+addBox(AvatarAnchorPointType.AAPT_LEFT_UP_LEG , Color4.create(0.25, 0.5, 0.25, 1))
+addBox(AvatarAnchorPointType.AAPT_LEFT_LEG , Color4.create(0.5, 0.25, 0.25, 1))
+addBox(AvatarAnchorPointType.AAPT_LEFT_FOOT , Color4.create(0.25, 0.25, 0.5, 1))
+addBox(AvatarAnchorPointType.AAPT_LEFT_TOE_BASE , Color4.create(0.5, 0.25, 0.5, 1))
+addBox(AvatarAnchorPointType.AAPT_RIGHT_UP_LEG , Color4.create(0.25, 0.5, 0.5, 1))
+addBox(AvatarAnchorPointType.AAPT_RIGHT_LEG , Color4.create(0.75, 0.75, 0.0, 1))
+addBox(AvatarAnchorPointType.AAPT_RIGHT_FOOT , Color4.create(0.75, 0.0, 0.75, 1))
+addBox(AvatarAnchorPointType.AAPT_RIGHT_TOE_BASE, Color4.create(0.3, 0.3, 0.3, 1))
