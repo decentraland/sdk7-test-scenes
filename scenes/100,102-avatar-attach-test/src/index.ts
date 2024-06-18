9559173a3a953305987db78dae05ad9cfe610e7b
Alejandro Alvarez Melucci <alejandro.alvarez@decentraland.org>
2024-06-18 10:59:28 -0300

    Modified the test scene to cycle between attach points for demo day

--- scenes/100,102-avatar-attach-test/src/index.ts
+++ scenes/100,102-avatar-attach-test/src/index.ts
@@ -1,48 +1,138 @@
 // We define the empty imports so the auto-complete feature works as expected.
 import { Color4 } from '@dcl/sdk/math'
-import { AvatarAnchorPointType, AvatarAttach, Material, MeshRenderer, Transform, engine } from '@dcl/sdk/ecs'
+import { AvatarAnchorPointType, AvatarAttach, ColliderLayer, Entity, GltfContainer, InputAction, Material, MeshRenderer, TextShape, Transform, engine, pointerEventsSystem } from '@dcl/sdk/ecs'
 
-function addBox(anchor: AvatarAnchorPointType, color: Color4) {
-  const box = engine.addEntity()
-  Transform.create(box,
+function createConsole():Entity {
+  const consoleStool = engine.addEntity()
+  Transform.create(consoleStool,
     {
-      position: { x: 0, y: 0, z: 0 },
-      scale: { x: 0.15, y: 0.15, z: 0.15 },
+      position: { x: 8, y: 0, z: 8 },
     })
 
-  MeshRenderer.setBox(box)
-  AvatarAttach.create(box, {
+  GltfContainer.create(consoleStool, {
+    src: "assets/CommandControl_01.glb"
+  })
+
+
+  pointerEventsSystem.onPointerDown(
+    {
+    entity: consoleStool,
+    opts: {
+        button: InputAction.IA_POINTER,
+        hoverText: 'Click'
+      }
+    },
+    function(){
+      cycleAttachPoints();
+    }
+      )
+
+
+const consoleText = engine.addEntity()
+Transform.create(consoleText,
+  {
+    position: { x: 0, y: 3, z: 0 },
+    rotation: {x: 0, y: 180, z: 0, w: 0},
+    parent: consoleStool
+  })
+
+TextShape.create(consoleText,{
+  text: "Click me"
+})
+return consoleText
+}
+
+
+function createParrot() : Entity{
+  const parrot = engine.addEntity()
+  Transform.create(parrot,
+    {
+      position: { x: 0, y: 1, z: 0 },
+      scale: { x: 0.3, y: 0.3, z: 0.3 },
+    })
+  GltfContainer.create(parrot, {
+    src: "assets/Parrot.glb",
+    invisibleMeshesCollisionMask : ColliderLayer.CL_NONE
+  })
+  return parrot;
+}
+
+let attachPoints = [
+  AvatarAnchorPointType.AAPT_POSITION,
+  AvatarAnchorPointType.AAPT_NAME_TAG,
+  AvatarAnchorPointType.AAPT_HEAD,
+  AvatarAnchorPointType.AAPT_NECK,
+  AvatarAnchorPointType.AAPT_SPINE,
+  AvatarAnchorPointType.AAPT_SPINE1,
+  AvatarAnchorPointType.AAPT_SPINE2,
+  AvatarAnchorPointType.AAPT_HIP,
+  AvatarAnchorPointType.AAPT_LEFT_SHOULDER,
+  AvatarAnchorPointType.AAPT_LEFT_ARM,
+  AvatarAnchorPointType.AAPT_LEFT_FOREARM,
+  AvatarAnchorPointType.AAPT_LEFT_HAND,
+  AvatarAnchorPointType.AAPT_LEFT_HAND_INDEX,
+  AvatarAnchorPointType.AAPT_RIGHT_SHOULDER,
+  AvatarAnchorPointType.AAPT_RIGHT_ARM,
+  AvatarAnchorPointType.AAPT_RIGHT_FOREARM,
+  AvatarAnchorPointType.AAPT_RIGHT_HAND,
+  AvatarAnchorPointType.AAPT_RIGHT_HAND_INDEX,
+  AvatarAnchorPointType.AAPT_LEFT_UP_LEG,
+  AvatarAnchorPointType.AAPT_LEFT_LEG,
+  AvatarAnchorPointType.AAPT_LEFT_FOOT,
+  AvatarAnchorPointType.AAPT_LEFT_TOE_BASE,
+  AvatarAnchorPointType.AAPT_RIGHT_UP_LEG,
+  AvatarAnchorPointType.AAPT_RIGHT_LEG,
+  AvatarAnchorPointType.AAPT_RIGHT_FOOT,
+  AvatarAnchorPointType.AAPT_RIGHT_TOE_BASE,
+]
+
+let attachPointsNames = [
+  "POSITION",
+  "NAME_TAG",
+  "HEAD",
+  "NECK",
+  "SPINE",
+  "SPINE1",
+  "SPINE2",
+  "HIP",
+  "LEFT_SHOULDER",
+  "LEFT_ARM",
+  "LEFT_FOREARM",
+  "LEFT_HAND",
+  "LEFT_HAND_INDEX",
+  "RIGHT_SHOULDER",
+  "RIGHT_ARM",
+  "RIGHT_FOREARM",
+  "RIGHT_HAND",
+  "RIGHT_HAND_INDEX",
+  "LEFT_UP_LEG",
+  "LEFT_LEG",
+  "LEFT_FOOT",
+  "LEFT_TOE_BASE",
+  "RIGHT_UP_LEG",
+  "RIGHT_LEG",
+  "RIGHT_FOOT",
+  "RIGHT_TOE_BASE",
+]
+
+let currentAttachPoint = 0;
+
+function cycleAttachPoints():void{
+
+  currentAttachPoint++
+  if(currentAttachPoint > attachPoints.length -1)
+    currentAttachPoint = 0
+
+  const anchor = attachPoints[currentAttachPoint];
+
+  AvatarAttach.createOrReplace(parrot, {
     anchorPointId: anchor
   })
 
-  Material.setBasicMaterial(box, {
-    diffuseColor: color,
-  })
+  TextShape.getMutable(consoleText).text = attachPointsNames[currentAttachPoint];
 }
 
-addBox(AvatarAnchorPointType.AAPT_POSITION, Color4.create(1.0, 0.0, 0.0, 1))
-addBox(AvatarAnchorPointType.AAPT_NAME_TAG, Color4.create(0.0, 1.0, 0.0, 1))
-addBox(AvatarAnchorPointType.AAPT_HEAD, Color4.create(0.0, 0.0, 1.0, 1))
-addBox(AvatarAnchorPointType.AAPT_NECK, Color4.create(1.0, 1.0, 0.0, 1))
-addBox(AvatarAnchorPointType.AAPT_SPINE, Color4.create(1.0, 0.0, 1.0, 1))
-addBox(AvatarAnchorPointType.AAPT_SPINE1, Color4.create(0.0, 1.0, 1.0, 1))
-addBox(AvatarAnchorPointType.AAPT_SPINE2, Color4.create(0.5, 0.0, 0.0, 1))
-addBox(AvatarAnchorPointType.AAPT_HIP, Color4.create(0.0, 0.5, 0.0, 1))
-addBox(AvatarAnchorPointType.AAPT_LEFT_SHOULDER , Color4.create(0.0, 0.0, 0.5, 1))
-addBox(AvatarAnchorPointType.AAPT_LEFT_ARM , Color4.create(0.5, 0.5, 0.0, 1))
-addBox(AvatarAnchorPointType.AAPT_LEFT_FOREARM , Color4.create(0.5, 0.0, 0.5, 1))
-addBox(AvatarAnchorPointType.AAPT_LEFT_HAND, Color4.create(0.0, 0.5, 0.5, 1))
-addBox(AvatarAnchorPointType.AAPT_LEFT_HAND_INDEX , Color4.create(0.75, 0.25, 0.5, 1))
-addBox(AvatarAnchorPointType.AAPT_RIGHT_SHOULDER , Color4.create(0.25, 0.75, 0.5, 1))
-addBox(AvatarAnchorPointType.AAPT_RIGHT_ARM , Color4.create(0.5, 0.25, 0.75, 1))
-addBox(AvatarAnchorPointType.AAPT_RIGHT_FOREARM , Color4.create(0.25, 0.5, 0.75, 1))
-addBox(AvatarAnchorPointType.AAPT_RIGHT_HAND, Color4.create(0.75, 0.5, 0.25, 1))
-addBox(AvatarAnchorPointType.AAPT_RIGHT_HAND_INDEX , Color4.create(0.5, 0.75, 0.25, 1))
-addBox(AvatarAnchorPointType.AAPT_LEFT_UP_LEG , Color4.create(0.25, 0.5, 0.25, 1))
-addBox(AvatarAnchorPointType.AAPT_LEFT_LEG , Color4.create(0.5, 0.25, 0.25, 1))
-addBox(AvatarAnchorPointType.AAPT_LEFT_FOOT , Color4.create(0.25, 0.25, 0.5, 1))
-addBox(AvatarAnchorPointType.AAPT_LEFT_TOE_BASE , Color4.create(0.5, 0.25, 0.5, 1))
-addBox(AvatarAnchorPointType.AAPT_RIGHT_UP_LEG , Color4.create(0.25, 0.5, 0.5, 1))
-addBox(AvatarAnchorPointType.AAPT_RIGHT_LEG , Color4.create(0.75, 0.75, 0.0, 1))
-addBox(AvatarAnchorPointType.AAPT_RIGHT_FOOT , Color4.create(0.75, 0.0, 0.75, 1))
-addBox(AvatarAnchorPointType.AAPT_RIGHT_TOE_BASE, Color4.create(0.3, 0.3, 0.3, 1))
+
+let consoleText = createConsole()
+let parrot = createParrot()
+
