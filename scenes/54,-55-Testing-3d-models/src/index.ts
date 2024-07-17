b5ee115fa21b60aa395f20faf30364449a4b47bb
Alejandro Alvarez Melucci <alejandro.alvarez@decentraland.org>
2024-07-17 10:51:42 -0300

    Updated variable names

--- scenes/54,-55-Testing-3d-models/src/index.ts
+++ scenes/54,-55-Testing-3d-models/src/index.ts
@@ -4,20 +4,20 @@ import { Vector3 } from '@dcl/sdk/math'
 // export all the functions required to make the scene work
 export * from '@dcl/sdk'
 
-let samplescene = engine.addEntity()
+let rose = engine.addEntity()
 
-GltfContainer.create(samplescene, { src: 'models/Rose.glb' })
+GltfContainer.create(rose, { src: 'models/Rose.glb' })
 
-Transform.create(samplescene, {
+Transform.create(rose, {
   position: Vector3.create(3, 0.5, 3),
   scale: Vector3.create(2, 2, 2)
 })
 
-let sampleCube = engine.addEntity()
+let table = engine.addEntity()
 
-Transform.create(sampleCube, {
+Transform.create(table, {
   position: Vector3.create(3, 0.1, 3),
   scale: Vector3.create(3, 0.2, 3)
 })
 
-MeshRenderer.setBox(sampleCube)
+MeshRenderer.setBox(table)
