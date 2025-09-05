8a2518fe4d9676f480d9e88b440e3566e10879a3
Pravus <pravusjif@gmail.com>
2025-09-05 02:40:07 +0200

    system to detect result

--- scenes/75,-9-trigger-areas/src/index.ts
+++ scenes/75,-9-trigger-areas/src/index.ts
@@ -2,16 +2,53 @@ import {
     engine,
     Transform,
     MeshRenderer,
-    TriggerArea
-} from '@dcl/sdk/ecs'
+    Material,
+    TriggerArea,
+    TriggerAreaResult,
+    AvatarModifierArea,
+    AvatarModifierType
+} from '@dcl/ecs'
+// } from '@dcl/sdk/ecs'
 import { Vector3, Color4, Quaternion } from '@dcl/sdk/math'
 
 export function main() {
     const triggerAreaEntity = engine.addEntity()
     Transform.create(triggerAreaEntity, {
-        position: Vector3.create(8, 1, 8),
+        position: Vector3.create(8, 2, 8),
+        scale: Vector3.create(4, 4, 4),
     })
     MeshRenderer.setBox(triggerAreaEntity)
+    Material.setPbrMaterial(triggerAreaEntity, {
+        albedoColor: Color4.create(1, 1, 1, 0.5),
+    })
     TriggerArea.create(triggerAreaEntity)
-    console.log('put TriggerArea component on entity...')
+    console.log('pravs - put TriggerArea component on entity...')
+    
+    function resultSystem() {
+        for (const [entity, triggerAreaResult] of engine.getEntitiesWith(TriggerAreaResult)) {
+            console.log(`pravs - DETECTED TriggerAreaResult on Entity: ${triggerAreaResult.triggeredEntity}`)
+            
+            if (triggerAreaResult.triggeredEntity === triggerAreaEntity) {
+                console.log('pravs - Triggered Entity is SAME as Area entity! =)')                
+                engine.removeSystem(resultSystem)   
+            }
+        }
+    }
+    engine.addSystem(resultSystem)
+    
+    //...................
+    /*const avatarModifierAreaEntity = engine.addEntity()
+    const avatarModifierAreaSize = Vector3.create(4, 4, 4)
+    const avatarModifierAreaSize2 = Vector3.create(2, 2, 2)
+    Transform.create(avatarModifierAreaEntity, {
+        position: Vector3.create(0, 1, 12),
+        scale: avatarModifierAreaSize2
+    })
+    AvatarModifierArea.create(avatarModifierAreaEntity, {
+        area: avatarModifierAreaSize,
+        modifiers: [AvatarModifierType.AMT_HIDE_AVATARS],
+        excludeIds: []
+    })
+    MeshRenderer.setBox(avatarModifierAreaEntity)
+    Material.setPbrMaterial(avatarModifierAreaEntity, { albedoColor: Color4.create(0.5, 0.5, 0.5, 0.5) })*/
 }
