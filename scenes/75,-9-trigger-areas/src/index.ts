17e714e70411606d5442acf0d48a7a277fcdee72
Pravus <pravusjif@gmail.com>
2025-09-18 14:35:12 +0200

    updated test code

--- scenes/75,-9-trigger-areas/src/index.ts
+++ scenes/75,-9-trigger-areas/src/index.ts
@@ -1,28 +1,36 @@
-import { engine,
+import {
+    ColliderLayer,
+    engine,
+    InputAction,
     Material,
+    MeshCollider,
     MeshRenderer,
+    pointerEventsSystem,
     Transform,
     TriggerArea,
-    TriggerAreaMeshType,
-    ColliderLayer,
     triggerAreaEventsSystem
 } from '@dcl/ecs'
 // } from '@dcl/sdk/ecs'
 import {Color4, Vector3} from '@dcl/sdk/math'
 
 export function main() {
-    const triggerAreaEntity = engine.addEntity()
+    const triggerAreaEntity = engine.addEntity() 
     Transform.create(triggerAreaEntity, {
         position: Vector3.create(8, 2, 8),
         scale: Vector3.create(4, 4, 4),
     })
-    MeshRenderer.setBox(triggerAreaEntity)
     Material.setPbrMaterial(triggerAreaEntity, {
         albedoColor: Color4.create(1, 1, 1, 0.5),
     })
 
-    TriggerArea.setSphere(triggerAreaEntity, ColliderLayer.CL_PLAYER)
+    // TriggerArea.setSphere(triggerAreaEntity, ColliderLayer.CL_PLAYER)
+    // MeshRenderer.setSphere(triggerAreaEntity)
+    // TriggerArea.setBox(triggerAreaEntity)
     // TriggerArea.setBox(triggerAreaEntity, ColliderLayer.CL_PLAYER)
+    TriggerArea.setBox(triggerAreaEntity, ColliderLayer.CL_CUSTOM3 | ColliderLayer.CL_PLAYER)
+    // TriggerArea.setBox(triggerAreaEntity, ColliderLayer.CL_CUSTOM4 | ColliderLayer.CL_CUSTOM3)
+    // TriggerArea.setBox(triggerAreaEntity, ColliderLayer.CL_CUSTOM1 | ColliderLayer.CL_CUSTOM5)
+    MeshRenderer.setBox(triggerAreaEntity)
 
     triggerAreaEventsSystem.onTriggerEnter(triggerAreaEntity,
         function (result) {
@@ -33,7 +41,7 @@ export function main() {
             console.log(`result.eventType: ${result.eventType}`)
             console.log(`result.timestamp: ${result.timestamp}`)
             console.log(`result.trigger.entity: ${result.trigger!.entity}`)
-            console.log(`result.trigger.layer: ${result.trigger!.layer}`)
+            console.log(`result.trigger.layer: ${result.trigger!.layer as ColliderLayer}`)
             console.log(`result.trigger.position: (${result.trigger!.position!.x}, ${result.trigger!.position!.y}, ${result.trigger!.position!.z})`)
             console.log(`result.trigger.rotation: (${result.trigger!.rotation!.x}, ${result.trigger!.rotation!.y}, ${result.trigger!.rotation!.z}, ${result.trigger!.rotation!.w})`)
             console.log(`result.trigger.scale: (${result.trigger!.scale!.x}, ${result.trigger!.scale!.y}, ${result.trigger!.scale!.z})`)
@@ -41,7 +49,7 @@ export function main() {
             // console.log(`pravs - TRIGGER IS Entity: ${result.trigger!.entity}`) // WHY IS result.trigger possibly undefined ???
         })
 
-    triggerAreaEventsSystem.onTriggerStay(triggerAreaEntity,
+/*    triggerAreaEventsSystem.onTriggerStay(triggerAreaEntity,
         function (result) {
             console.log(`pravs - DETECTED OnSTAY...`)
             console.log(`result.triggeredEntity: ${result.triggeredEntity}`)
@@ -50,11 +58,11 @@ export function main() {
             console.log(`result.eventType: ${result.eventType}`)
             console.log(`result.timestamp: ${result.timestamp}`)
             console.log(`result.trigger.entity: ${result.trigger!.entity}`)
-            console.log(`result.trigger.layer: ${result.trigger!.layer}`)
+            console.log(`result.trigger.layer: ${result.trigger!.layer as ColliderLayer}`)
             console.log(`result.trigger.position: (${result.trigger!.position!.x}, ${result.trigger!.position!.y}, ${result.trigger!.position!.z})`)
             console.log(`result.trigger.rotation: (${result.trigger!.rotation!.x}, ${result.trigger!.rotation!.y}, ${result.trigger!.rotation!.z}, ${result.trigger!.rotation!.w})`)
             console.log(`result.trigger.scale: (${result.trigger!.scale!.x}, ${result.trigger!.scale!.y}, ${result.trigger!.scale!.z})`)
-        })
+        })*/
 
     triggerAreaEventsSystem.onTriggerExit(triggerAreaEntity,
         function (result) {
@@ -65,13 +73,32 @@ export function main() {
             console.log(`result.eventType: ${result.eventType}`)
             console.log(`result.timestamp: ${result.timestamp}`)
             console.log(`result.trigger.entity: ${result.trigger!.entity}`)
-            console.log(`result.trigger.layer: ${result.trigger!.layer}`)
+            console.log(`result.trigger.layer: ${result.trigger!.layer as ColliderLayer}`)
             console.log(`result.trigger.position: (${result.trigger!.position!.x}, ${result.trigger!.position!.y}, ${result.trigger!.position!.z})`)
             console.log(`result.trigger.rotation: (${result.trigger!.rotation!.x}, ${result.trigger!.rotation!.y}, ${result.trigger!.rotation!.z}, ${result.trigger!.rotation!.w})`)
             console.log(`result.trigger.scale: (${result.trigger!.scale!.x}, ${result.trigger!.scale!.y}, ${result.trigger!.scale!.z})`)
         })
-    
-    
+
+    const entityTrigger = engine.addEntity()
+    Transform.create(entityTrigger, { position: Vector3.create(4, 1, 8), scale: Vector3.create(0.25, 0.25, 0.25)})
+    MeshRenderer.setBox(entityTrigger)
+    // MeshCollider.setBox(entityTrigger, ColliderLayer.CL_PLAYER)
+    MeshCollider.setBox(entityTrigger, ColliderLayer.CL_CUSTOM3)
+
+    const entityTriggerButton = engine.addEntity()
+    Transform.create(entityTriggerButton, { position: Vector3.create(2, 1, 2)})
+    MeshRenderer.setBox(entityTriggerButton)
+    MeshCollider.setBox(entityTriggerButton)
+    pointerEventsSystem.onPointerDown(
+        { entity: entityTriggerButton, opts: { button: InputAction.IA_POINTER, hoverText: 'move trigger' } },
+        () => {
+            const mutableTransform = Transform.getMutable(entityTrigger)
+            if (mutableTransform.position.x == 4)
+                mutableTransform.position.x = 8
+            else
+                mutableTransform.position.x = 4
+        }
+    )
     
       
     //...................
