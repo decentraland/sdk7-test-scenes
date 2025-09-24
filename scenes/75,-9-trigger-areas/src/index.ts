0ea71d06d7125de94a5764775447191435b6daa6
Pravus <pravusjif@gmail.com>
2025-09-24 03:03:15 +0200

    added a couple more test cases

--- scenes/75,-9-trigger-areas/src/index.ts
+++ scenes/75,-9-trigger-areas/src/index.ts
@@ -1,6 +1,7 @@
 import {
     ColliderLayer,
     engine,
+    Entity,
     InputAction,
     Material,
     MeshCollider,
@@ -9,28 +10,60 @@ import {
     Transform,
     TriggerArea,
     triggerAreaEventsSystem,
-    Entity
+    GltfContainer
 } from '@dcl/ecs'
-import { Color4, Vector3, Quaternion } from '@dcl/sdk/math'
+import {Color4, Quaternion, Vector3} from '@dcl/sdk/math'
+import * as CANNON from 'cannon'
+
+// Cannon.js physics world setup
+const world = new CANNON.World()
+world.gravity.set(0, -9.82, 0)
+
+const physicsBodies = new Map<Entity, CANNON.Body>()
+const fixedTimeStep = 1 / 60
+let physicsAccumulator = 0
 
 export function main() {
-    const entityTrigger = engine.addEntity()
-    Transform.create(entityTrigger, { position: Vector3.create(3, 1, 14.5), scale: Vector3.create(0.25, 0.25, 0.25)})
-    MeshRenderer.setBox(entityTrigger)
-    MeshCollider.setBox(entityTrigger, ColliderLayer.CL_CUSTOM3)
+    // Box Trigger Area detecting Entity and Player
+    const movingCube = engine.addEntity()
+    Transform.create(movingCube, { position: Vector3.create(3, 1, 14.5), scale: Vector3.create(0.25, 0.25, 0.25)})
+    MeshRenderer.setBox(movingCube)
+    MeshCollider.setBox(movingCube, ColliderLayer.CL_CUSTOM3)
 
-    const entityTriggerButton = engine.addEntity()
-    Transform.create(entityTriggerButton, { position: Vector3.create(1, 1, 14.5)})
-    MeshRenderer.setBox(entityTriggerButton)
-    MeshCollider.setBox(entityTriggerButton)
+    const cubeMovementEntity = engine.addEntity()
+    Transform.create(cubeMovementEntity, { position: Vector3.create(1, 1, 14.5)})
+    MeshRenderer.setBox(cubeMovementEntity)
+    MeshCollider.setBox(cubeMovementEntity)
     pointerEventsSystem.onPointerDown(
-        { entity: entityTriggerButton, opts: { button: InputAction.IA_POINTER, hoverText: 'move trigger' } },
+        { entity: cubeMovementEntity, opts: { button: InputAction.IA_POINTER, hoverText: 'move trigger' } },
         () => {
-            const mutableTransform = Transform.getMutable(entityTrigger)
+            const mutableTransform = Transform.getMutable(movingCube)
             if (mutableTransform.position.x == 3)
                 mutableTransform.position.x = 5
             else
                 mutableTransform.position.x = 3
+        }
+    )
+
+    const movingGltfContainer = engine.addEntity()
+    Transform.create(movingGltfContainer, { position: Vector3.create(8, 2, 14.5)})
+    GltfContainer.create(movingGltfContainer, {
+        src: 'models/Monster.glb',
+        visibleMeshesCollisionMask: ColliderLayer.CL_CUSTOM4
+    })
+
+    const gltfMovementEntity = engine.addEntity()
+    Transform.create(gltfMovementEntity, { position: Vector3.create(10, 1, 14.5)})
+    MeshRenderer.setBox(gltfMovementEntity)
+    MeshCollider.setBox(gltfMovementEntity)
+    pointerEventsSystem.onPointerDown(
+        { entity: gltfMovementEntity, opts: { button: InputAction.IA_POINTER, hoverText: 'move trigger' } },
+        () => {
+            const mutableTransform = Transform.getMutable(movingGltfContainer)
+            if (mutableTransform.position.x == 8)
+                mutableTransform.position.x = 5
+            else
+                mutableTransform.position.x = 8
         }
     )
 
@@ -39,8 +72,69 @@ export function main() {
         position: Vector3.create(5, 1.5, 14.5),
         scale: Vector3.create(1, 3, 1),
         rotation: Quaternion.fromEulerDegrees(0, 45, 0)
-    })    
-    setupTriggerArea(triggerAreaEntity, ColliderLayer.CL_PLAYER | ColliderLayer.CL_CUSTOM3)
+    })
+    setupTriggerArea(triggerAreaEntity, ColliderLayer.CL_PLAYER | ColliderLayer.CL_CUSTOM4)
+    
+    // Spawn falling sphere with physics that gets removed on trigger enter
+    const sphereSpawnButton = engine.addEntity()
+    Transform.create(sphereSpawnButton, { position: Vector3.create(8, 1, 10.5) })
+    MeshRenderer.setSphere(sphereSpawnButton)
+    MeshCollider.setSphere(sphereSpawnButton)
+    addSpherePhysics(sphereSpawnButton, 0.5, 0)
+    pointerEventsSystem.onPointerDown(
+        { entity: sphereSpawnButton, opts: { button: InputAction.IA_POINTER, hoverText: 'spawn sphere' } },
+        () => {
+            const buttonTransform = Transform.get(sphereSpawnButton)
+
+            const spawnedSphere = engine.addEntity()
+            Transform.create(spawnedSphere, {
+                position: Vector3.create(
+                    buttonTransform.position.x,
+                    buttonTransform.position.y + 2,
+                    buttonTransform.position.z
+                ),
+                scale: Vector3.create(1, 1, 1)
+            })
+
+            MeshRenderer.setSphere(spawnedSphere)
+            MeshCollider.setSphere(spawnedSphere, ColliderLayer.CL_PHYSICS)
+            Material.setPbrMaterial(spawnedSphere, {
+                albedoColor: Color4.create(1, 1, 1, 0.5)
+            })
+
+            TriggerArea.setSphere(spawnedSphere, ColliderLayer.CL_POINTER)
+
+            // Add physics so it falls with gravity
+            addSpherePhysics(spawnedSphere, 0.5, 1)
+
+            // Remove sphere on first OnEnter trigger
+            triggerAreaEventsSystem.onTriggerEnter(spawnedSphere, () => {
+                const body = physicsBodies.get(spawnedSphere)
+                if (body) {
+                    world.remove(body)
+                    physicsBodies.delete(spawnedSphere)
+                }
+                engine.removeEntity(spawnedSphere)
+            })
+        }
+    )
+
+    // Physics stepping and syncing system
+    engine.addSystem((dt: number) => {
+        physicsAccumulator += dt
+        while (physicsAccumulator >= fixedTimeStep) {
+            world.step(fixedTimeStep)
+            physicsAccumulator -= fixedTimeStep
+        }
+
+        physicsBodies.forEach((body, entity) => {
+            const mutableTransform = Transform.getMutable(entity)
+            mutableTransform.position.x = body.position.x
+            mutableTransform.position.y = body.position.y
+            mutableTransform.position.z = body.position.z
+        })
+    })
+
 }
 
 function setupTriggerArea(triggerAreaEntity: Entity, collisionMask: number | undefined, isSphere = false) {
@@ -59,6 +153,11 @@ function setupTriggerArea(triggerAreaEntity: Entity, collisionMask: number | und
     triggerAreaEventsSystem.onTriggerEnter(triggerAreaEntity,
         function (result) {
             console.log(`${triggerAreaEntity} DETECTED OnEnter from other entity: ${result.trigger!.entity}`)
+            const randomColor = Color4.fromHexString(getRandomHexColor())
+            randomColor.a = 0.5
+            Material.setPbrMaterial(triggerAreaEntity, {
+                albedoColor: randomColor,
+            })
 /*            console.log(`result.triggeredEntity: ${result.triggeredEntity}`)
             console.log(`result.triggeredEntityPosition: (${result.triggeredEntityPosition!.x}, ${result.triggeredEntityPosition!.y}, ${result.triggeredEntityPosition!.z})`)
             console.log(`result.triggeredEntityRotation: (${result.triggeredEntityRotation!.x}, ${result.triggeredEntityRotation!.y}, ${result.triggeredEntityRotation!.z}, ${result.triggeredEntityRotation!.w})`)
@@ -79,5 +178,28 @@ function setupTriggerArea(triggerAreaEntity: Entity, collisionMask: number | und
     triggerAreaEventsSystem.onTriggerExit(triggerAreaEntity,
         function (result) {
             console.log(`${triggerAreaEntity} DETECTED OnExit from other entity: ${result.trigger!.entity}`)
+            const randomColor = Color4.fromHexString(getRandomHexColor())
+            randomColor.a = 0.5
+            Material.setPbrMaterial(triggerAreaEntity, {
+                albedoColor: randomColor,
+            })
         })
+}
+
+function addSpherePhysics(entity: Entity, radius: number, mass = 1) {
+    const transform = Transform.get(entity)
+    const shape = new CANNON.Sphere(radius)
+    const body = new CANNON.Body({ mass, shape })
+    body.position.set(transform.position.x, transform.position.y, transform.position.z)
+    world.addBody(body)
+    physicsBodies.set(entity, body)
+}
+
+export function getRandomHexColor(): string {
+    const letters = "0123456789ABCDEF";
+    let color = "#";
+    for (let i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
 }
