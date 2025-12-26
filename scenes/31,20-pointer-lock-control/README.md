1bd1023f41e86158e0aad7baa595bc503aadb5a9
Pravus <pravusjif@gmail.com>
2025-12-26 17:12:34 +0100

    chore: pointer-lock control test scene (#43)

---
+++ scenes/31,20-pointer-lock-control/README.md
@@ -0,0 +1,9 @@
+The scene will lock the pointer every 10 seconds.
+
+You can also lock the pointer by interacting with the first cube.
+
+You can unlock the pointer by interacting with the second cube.
+
+If you open the explore panel or any other full screen UI, and the scene requested to lock the pointer, the pointer should not lock until you come back to the scene.
+
+If you open the developer console the pointer lock status should be printed correctly.
---
+++ scenes/31,20-pointer-lock-control/src/index.ts
@@ -0,0 +1,61 @@
+import {
+    engine,
+    Entity,
+    PointerLock,
+    pointerEventsSystem,
+    Transform,
+    MeshRenderer,
+    MeshCollider,
+    InputAction,
+    EventSystemCallback
+} from '@dcl/sdk/ecs'
+import {Vector3} from '@dcl/sdk/math';
+
+let lockPointerTimer = 0;
+
+export async function main() {
+    PointerLock.createOrReplace(engine.CameraEntity, {isPointerLocked: false});
+    
+    PointerLock.onChange(engine.CameraEntity, (pointerLock) => {
+        if (!pointerLock) return;
+        console.log(`Is pointer locked: ${pointerLock?.isPointerLocked}`);
+    });
+
+    createTestBox(Vector3.create(6, 1, 8), 'Lock', () => {
+        PointerLock.getMutable(engine.CameraEntity).isPointerLocked = true;
+    });
+
+    createTestBox(Vector3.create(10, 1, 8), 'Unlock', () => {
+        PointerLock.getMutable(engine.CameraEntity).isPointerLocked = false;
+    });
+
+    engine.addSystem(lockPointerAfterDelay);
+}
+
+function lockPointerAfterDelay(time: number) {
+    lockPointerTimer += time;
+    
+    if (lockPointerTimer >= 10) {
+        PointerLock.getMutable(engine.CameraEntity).isPointerLocked = true;
+        lockPointerTimer = 0;
+    }
+}
+
+function createTestBox(position: Vector3, hoverText: string, onClickCallback: EventSystemCallback) {
+    const box = engine.addEntity()
+
+    Transform.create(box, {
+        position: position,
+    })
+
+    MeshRenderer.setBox(box)
+    MeshCollider.setBox(box)
+
+    pointerEventsSystem.onPointerDown(
+        {
+            entity: box,
+            opts: {button: InputAction.IA_POINTER, hoverText: hoverText},
+        },
+        onClickCallback
+    )
+}
