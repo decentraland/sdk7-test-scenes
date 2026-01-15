832c52a4e16dc3c4112915ab9bee9ec72136055e
lorenzo-ranciaffi <ranciaffi.lorenzo@gmail.com>
2026-01-15 15:52:30 +0100

    moved code

--- scenes/88,-12-asset-load/src/index.ts
+++ scenes/88,-12-asset-load/src/index.ts
@@ -1,105 +1,69 @@
 import {
   AssetLoad,
-  AudioSource,
-  Billboard,
-  BillboardMode,
+  AssetLoadLoadingState,
   engine,
   Entity,
-  GltfContainer,
-  GltfContainerLoadingState,
-  LoadingState,
   Material,
-  MeshRenderer,
-  Transform,
-  VideoPlayer,
 } from '@dcl/sdk/ecs'
-import { Color4, Vector3 } from '@dcl/sdk/math'
-import { createClickableCube } from './utils'
+import { Vector3 } from '@dcl/sdk/math'
+import { createClickableCube, handleAudio, handleTexture, handleVideo, handleGLB, basePosition, mp3Path, texturePath, videoPath, glbPath, getLoadingColor } from './utils'
 
+
+let assetLoadCube: Entity
+let audioCube: Entity
+let textureCube: Entity
+let videoCube: Entity
+let glbCube: Entity
 
 export function main() {
+  assetLoadCube = createClickableCube(Vector3.add(basePosition, Vector3.create(3, 0, -4)), 'Preload Assets', (cube) => {
+    AssetLoad.getOrCreateMutable(cube, {assets: [mp3Path, texturePath, videoPath, glbPath]})
+  })
+
   // Audio Cube
-  handleAudio()
+  audioCube = handleAudio()
 
   // Text Cube
-  handleTexture()
+  textureCube = handleTexture()
 
   // Video Screen
-  handleVideo()
+  videoCube = handleVideo()
 
   // GLB Model
-  handleGLB()
+  glbCube = handleGLB()
+
+  engine.addSystem(assetLoadingStateSystem)
 }
 
-function handleAudio(){
-  createClickableCube(Vector3.create(0, 1, 0), 'Play/Pause MP3', (cube) => {
-    let audioSource = AudioSource.getOrCreateMutable(cube, {
-      audioClipUrl: 'assets/scene/Audio/Vexento.mp3',
-      playing: false,
-      volume: 1.0
-    })
-    audioSource.playing = !audioSource.playing
-  })
-}
+function assetLoadingStateSystem(dt: number){
+  const loadingState = AssetLoadLoadingState.getOrNull(assetLoadCube)
 
-function handleTexture(){
-  const textureCube = engine.addEntity()
-  Transform.create(textureCube, {
-    position: Vector3.create(2, 1, 2),
-    scale: Vector3.create(1.5, 1.5, 1)
-  })
-  MeshRenderer.setPlane(textureCube)
-  Billboard.create(textureCube, {billboardMode: BillboardMode.BM_Y});
+  if (!loadingState){
+    return
+  }
 
-  createClickableCube(Vector3.create(2, 1, 0), 'Set texture', (cube) => {
-    Material.setPbrMaterial(textureCube, {
-      albedoColor: Color4.create(1, 1, 1, 1),
-      texture: Material.Texture.Common({
-        src: 'assets/scene/Images/Logo.png'
-      })
-  });
+  const cube = getCube(loadingState.asset)
+  if (!cube){
+    return
+  }
+
+  Material.setPbrMaterial(cube, {
+    albedoColor: getLoadingColor(loadingState.currentState)
   })
 }
 
-function handleVideo(){
-  const videoScreen = engine.addEntity()
-  Transform.create(videoScreen, {
-    position: Vector3.create(4, 1, 2),
-    scale: Vector3.create(1.5, 1.5, 1)
-  })
-  MeshRenderer.setPlane(videoScreen)
-  Billboard.create(videoScreen, {billboardMode: BillboardMode.BM_Y});
-
-  const videoTexture = Material.Texture.Video({ videoPlayerEntity: videoScreen })
-  Material.setBasicMaterial(videoScreen, {
-    texture: videoTexture
-  })
-
-  
-  createClickableCube(Vector3.create(4, 1, 0), 'Play/Pause Video', (cube) => {
-    let video = VideoPlayer.getOrCreateMutable(videoScreen, {
-      src: 'assets/scene/Video/video-example.mp4',
-      playing: false,
-      loop: true,
-      volume: 1.0,
-      playbackRate: 1.0,
-      position: 0  // Start time in seconds
-    })
-    video.playing = !video.playing
-  })
+function getCube(assetPath: string): Entity | null {
+  switch (assetPath) {
+    case mp3Path:
+      return audioCube
+    case texturePath:
+      return textureCube
+    case videoPath:
+      return videoCube
+    case glbPath:
+      return glbCube
+  }
+  return null
 }
 
-function handleGLB(){
-  const glbModel = engine.addEntity()
-  
-  Transform.create(glbModel, {
-    position: Vector3.create(6, 1, 2),
-    scale: Vector3.create(1, 1, 1)
-  })
 
-  createClickableCube(Vector3.create(6, 1, 0), 'Spawn GLB', (cube) => {
-    GltfContainer.create(glbModel, {
-      src: 'assets/scene/Models/chicken.glb'
-    })
-  })
-}
--- scenes/88,-12-asset-load/src/utils.ts
+++ scenes/88,-12-asset-load/src/utils.ts
@@ -5,9 +5,41 @@ import {
     MeshCollider,
     pointerEventsSystem,
     InputAction,
-    Entity
+    Entity,
+    Material,
+    AudioSource,
+    Billboard,
+    BillboardMode,
+    VideoPlayer,
+    GltfContainer,
+    LoadingState
   } from '@dcl/sdk/ecs'
 import { Vector3, Color4, Quaternion } from "@dcl/sdk/math"
+
+const unloadedColor = Color4.create(1, 1, 1, 1);
+const loadedColor = Color4.create(0, 1, 0, 1);
+const loadingColor = Color4.create(1, 1, 0, 1);
+const loadingErrorColor = Color4.create(1, 0, 0, 1);
+
+export const basePosition = Vector3.create(4, 1, 8);
+
+export const mp3Path = 'assets/scene/Audio/Vexento.mp3';
+export const texturePath = 'assets/scene/Images/Logo.png';
+export const videoPath = 'assets/scene/Video/video-example.mp4';
+export const glbPath = 'assets/scene/Models/chicken.glb';
+
+export function getLoadingColor(loadingState: LoadingState): Color4 {
+  switch (loadingState) {
+    case LoadingState.FINISHED:
+      return loadedColor
+    case LoadingState.LOADING:
+      return loadingColor
+    case LoadingState.FINISHED_WITH_ERROR:
+      return loadingErrorColor
+    default:
+      return unloadedColor
+  }
+}
 
 export function createClickableCube(
     position: Vector3,
@@ -18,6 +50,10 @@ export function createClickableCube(
   Transform.create(cube, { position: position })
   MeshRenderer.setBox(cube)
   MeshCollider.setBox(cube)
+
+  Material.setPbrMaterial(cube, {
+    albedoColor: unloadedColor
+  })
   
   pointerEventsSystem.onPointerDown({ entity: cube, opts: { button: InputAction.IA_POINTER, hoverText: hoverText, maxDistance:7 } }, () => {
     onClick(cube)
@@ -25,3 +61,83 @@ export function createClickableCube(
 
   return cube
 }
+
+export function handleAudio(){
+    let cube = createClickableCube(basePosition, 'Play/Pause MP3', (cube) => {
+      let audioSource = AudioSource.getOrCreateMutable(cube, {
+        audioClipUrl: mp3Path,
+        playing: false,
+        volume: 1.0
+      })
+      audioSource.playing = !audioSource.playing
+    })
+    return cube
+  }
+  
+  export function handleTexture(){
+    const textureCube = engine.addEntity()
+    Transform.create(textureCube, {
+      position: Vector3.add(basePosition, Vector3.create(2, 1, 2)),
+      scale: Vector3.create(1.5, 1.5, 1)
+    })
+    MeshRenderer.setPlane(textureCube)
+    Billboard.create(textureCube, {billboardMode: BillboardMode.BM_Y});
+  
+    let cube = createClickableCube(Vector3.add(basePosition, Vector3.create(2, 0, 0)), 'Set texture', (cube) => {
+      Material.setPbrMaterial(textureCube, {
+        albedoColor: Color4.create(1, 1, 1, 1),
+        texture: Material.Texture.Common({
+          src: texturePath
+        })
+    });
+    })
+  
+    return cube
+  }
+  
+  export function handleVideo(){
+    const videoScreen = engine.addEntity()
+    Transform.create(videoScreen, {
+      position: Vector3.add(basePosition, Vector3.create(4, 1, 2)),
+      scale: Vector3.create(1.5, 1.5, 1)
+    })
+    MeshRenderer.setPlane(videoScreen)
+    Billboard.create(videoScreen, {billboardMode: BillboardMode.BM_Y});
+  
+    const videoTexture = Material.Texture.Video({ videoPlayerEntity: videoScreen })
+    Material.setBasicMaterial(videoScreen, {
+      texture: videoTexture
+    })
+  
+    
+    let cube = createClickableCube(Vector3.add(basePosition, Vector3.create(4, 0, 0)), 'Play/Pause Video', (cube) => {
+      let video = VideoPlayer.create(videoScreen, {
+        src: videoPath,
+        playing: false,
+        loop: true,
+        volume: 1.0,
+        playbackRate: 1.0,
+        position: 0  // Start time in seconds
+      })
+      video.playing = !video.playing
+    })
+  
+    return cube
+  }
+  
+  export function handleGLB(){
+    const glbModel = engine.addEntity()
+    
+    Transform.create(glbModel, {
+      position: Vector3.add(basePosition, Vector3.create(6, 1, 2)),
+      scale: Vector3.create(1, 1, 1)
+    })
+  
+    let cube = createClickableCube(Vector3.add(basePosition, Vector3.create(6, 0, 0)), 'Spawn GLB', (cube) => {
+      GltfContainer.create(glbModel, {
+        src: glbPath
+      })
+    })
+  
+    return cube
+  }
