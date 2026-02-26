3072c00af4b6b3a4a0ee78249a2b06c11fdd425d
Vitaly Popuzin <popuzin@gmail.com>
2026-02-26 18:20:27 +0100

    stale stuffs

---
+++ scenes/0,7-physics-impulse/PhysicsForces/decentraland-sdk-empty-scene-template-2519f16/src/index.ts
@@ -0,0 +1,12 @@
+import {} from '@dcl/sdk/math'
+import { engine } from '@dcl/sdk/ecs'
+import { setupUi } from './ui'
+
+
+export function main() {
+    // uncomment the line below to initialize UI from ui.tsx
+    //setupUi()
+
+    // your scene code here
+}
+
