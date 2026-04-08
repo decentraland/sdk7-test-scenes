1a41ceaaa8c7c36dd1f0a3ee8ce8201fe032440a
Maurizio Farmi <maurizio.farmi@decentraland.org>
2026-04-08 12:00:59 +0200

    chore: update avatar masks scene by removing unused assets and updating scene configuration

--- scenes/88,-13-avatar-masks/assets/scene/entity-names.ts
+++ scenes/88,-13-avatar-masks/assets/scene/entity-names.ts
@@ -5,12 +5,5 @@
  * Object containing all entity names in the scene for autocomplete support.
  */
 export enum EntityNames {
-  Dock_Piling = "Dock Piling",
-  Dock_Piling_2 = "Dock Piling_2",
-  Dock_Piling_3 = "Dock Piling_3",
   Ground = "Ground",
-  Small_Straight_Dock_Module = "Small Straight Dock Module",
-  Small_Straight_Dock_Module_2 = "Small Straight Dock Module_2",
-  Tile_1 = "Tile 1",
-  Wooden_Dock_Stairs = "Wooden Dock Stairs",
 } 
