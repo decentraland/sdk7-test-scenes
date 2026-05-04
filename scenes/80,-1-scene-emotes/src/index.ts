45b727f323c171adec75f814375b22c844960db0
Maurizio Farmi <maurizio.farmi@decentraland.org>
2026-05-04 11:18:57 +0200

    Updated package.json dependencies, adjusted test cube positions and added masked scene emote cube

--- scenes/80,-1-scene-emotes/src/index.ts
+++ scenes/80,-1-scene-emotes/src/index.ts
@@ -1,23 +1,27 @@
 import { Vector3 } from '@dcl/sdk/math'
 import { triggerEmote, triggerSceneEmote } from '~system/RestrictedActions'
-import { engine, GltfContainer, Transform, pointerEventsSystem, MeshRenderer, MeshCollider, TransformType, EventSystemCallback } from '@dcl/sdk/ecs'
+import { engine, GltfContainer, Transform, pointerEventsSystem, MeshRenderer, MeshCollider, TransformType, EventSystemCallback, AvatarEmoteMask } from '@dcl/sdk/ecs'
 
-addTestCube({ position: Vector3.create(8, 1, 5) }, () => {
+addTestCube({ position: Vector3.create(8, 1, 2) }, () => {
   triggerEmote({ predefinedEmote: 'robot' })
 }, "robot")
 
-addTestCube({ position: Vector3.create(8, 1, 8) }, () => {
+addTestCube({ position: Vector3.create(8, 1, 4) }, () => {
   triggerSceneEmote({ src: 'animations/Crafting_Snowball_emote.glb', loop: false })
 }, "Crafting_Snowball_emote.glb")
 
 // Not working on purpose, because the naming doesn't have "_emote"
-addTestCube({ position: Vector3.create(8, 1, 11) }, () => {
+addTestCube({ position: Vector3.create(8, 1, 6) }, () => {
   triggerSceneEmote({ src: 'animations/Snowball_Throw.glb', loop: false })
 }, "Snowball_Throw.glb")
 
-addTestCube({ position: Vector3.create(8, 1, 14) }, () => {
+addTestCube({ position: Vector3.create(8, 1, 8) }, () => {
     triggerSceneEmote({ src: 'animations/LoveGrenade_emote.glb', loop: true })
 }, "LoveGrenade_emote.glb")
+
+addTestCube({ position: Vector3.create(8, 1, 10) }, () => {
+  triggerSceneEmote({ src: 'animations/Crafting_Snowball_emote.glb', loop: true, mask: AvatarEmoteMask.AEM_UPPER_BODY })
+}, "Crafting_Snowball_emote.glb (upper body)")
 
 let snowTree = engine.addEntity()
 
