6f400c088a127f5d322afe088c917f101a365ae7
Pravus <pravusjif@gmail.com>
2026-09-15 01:06:56 +0200

    chore: updated ui scene with uirenderer stacking (#97)

--- scenes/0,6-ui-zindex-and-opacity/src/index.ts
+++ scenes/0,6-ui-zindex-and-opacity/src/index.ts
@@ -1,9 +1,11 @@
 import { ReactEcsRenderer } from '@dcl/sdk/react-ecs'
 import { engine, InputAction, inputSystem, MainCamera, pointerEventsSystem, PointerEventType, PrimaryPointerInfo, RaycastQueryType, raycastSystem, VirtualCamera, Transform, TextShape, MeshRenderer, MeshCollider, Material, InputModifier, ColliderLayer } from '@dcl/sdk/ecs'
 import { uiMenu, changeGreenZIndex } from './ui'
+import { setupRendererStacking } from './renderer-stacking'
 
 export function main() {
     ReactEcsRenderer.setUiRenderer(uiMenu)
+    setupRendererStacking()
     engine.addSystem(() => {
         if (inputSystem.isTriggered(InputAction.IA_SECONDARY, PointerEventType.PET_DOWN)) {
             changeGreenZIndex()
