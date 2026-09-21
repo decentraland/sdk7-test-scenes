04b4bbb3e8f933deebb7fce17357f6c27e1bfe08
Pravus <pravusjif@gmail.com>
2026-09-21 20:19:57 +0200

    chore: update ui test scene rendering order (#101)

--- scenes/0,6-ui-zindex-and-opacity/src/index.ts
+++ scenes/0,6-ui-zindex-and-opacity/src/index.ts
@@ -2,10 +2,14 @@ import { ReactEcsRenderer } from '@dcl/sdk/react-ecs'
 import { engine, InputAction, inputSystem, MainCamera, pointerEventsSystem, PointerEventType, PrimaryPointerInfo, RaycastQueryType, raycastSystem, VirtualCamera, Transform, TextShape, MeshRenderer, MeshCollider, Material, InputModifier, ColliderLayer } from '@dcl/sdk/ecs'
 import { uiMenu, changeGreenZIndex } from './ui'
 import { setupRendererStacking } from './renderer-stacking'
+import { setupRawSiblings } from './raw-siblings'
+import { setupAdminToolkitStacking } from './admin-toolkit-stacking'
 
 export function main() {
     ReactEcsRenderer.setUiRenderer(uiMenu)
     setupRendererStacking()
+    setupRawSiblings()
+    setupAdminToolkitStacking()
     engine.addSystem(() => {
         if (inputSystem.isTriggered(InputAction.IA_SECONDARY, PointerEventType.PET_DOWN)) {
             changeGreenZIndex()
